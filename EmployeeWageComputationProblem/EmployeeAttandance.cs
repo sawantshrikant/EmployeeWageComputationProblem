be4af76d18a2f294ca9641d6c94d2c68911eb441
Shrikant <sawantshrikant07@gmail.com>
2023-03-10 15:07:23 +0530

    [Shrikant] add:DailyWage

--- EmployeeWageComputationProblem/EmployeeAttandance.cs
+++ EmployeeWageComputationProblem/EmployeeAttandance.cs
@@ -4,7 +4,7 @@ namespace EmployeeWageComputationProblem
 {
     public class EmployeeAttandance
     {
-        const int IS_PRESENT = 1;
+        const int IS_PRESENT = 1, WAGE_PER_HR = 20, NO_OF_HRS = 8;
         public void attandance()
         {
             Random random = new Random();
@@ -13,6 +13,15 @@ namespace EmployeeWageComputationProblem
                 Console.WriteLine("Employee is present");
             else
                 Console.WriteLine("Employee is absent");
+        }
+        public void DailyWage()
+        {
+            int wage = 0;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == IS_PRESENT)
+                wage = WAGE_PER_HR * NO_OF_HRS;
+            Console.WriteLine("Daily wage of an employee is: " + wage);
         }
     }
 }
