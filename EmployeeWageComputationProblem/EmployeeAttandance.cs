1af2e3e21d4c27a946d8d3d658e71256f4886aa5
Shrikant <sawantshrikant07@gmail.com>
2023-03-10 15:18:30 +0530

    [Shrikant] add:PartTimeWage

--- EmployeeWageComputationProblem/EmployeeAttandance.cs
+++ EmployeeWageComputationProblem/EmployeeAttandance.cs
@@ -4,7 +4,7 @@ namespace EmployeeWageComputationProblem
 {
     public class EmployeeAttandance
     {
-        const int IS_PRESENT = 1, WAGE_PER_HR = 20, NO_OF_HRS = 8;
+        const int IS_PRESENT = 1, WAGE_PER_HR = 20, NO_OF_HRS = 8, IS_FULL_TIME = 1, IS_PART_TIME = 2;
         public void attandance()
         {
             Random random = new Random();
@@ -18,10 +18,13 @@ namespace EmployeeWageComputationProblem
         {
             int wage = 0;
             Random random = new Random();
-            int check = random.Next(0, 2);
-            if (check == IS_PRESENT)
+            int check = random.Next(0, 3);
+            if (check == IS_FULL_TIME)
                 wage = WAGE_PER_HR * NO_OF_HRS;
+            if (check == IS_PART_TIME)
+                wage = WAGE_PER_HR * (NO_OF_HRS / 2);
             Console.WriteLine("Daily wage of an employee is: " + wage);
         }
+
     }
 }
