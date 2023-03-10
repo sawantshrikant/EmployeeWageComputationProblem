8a046394932b2d4da0bbb655a222af1ff43d2b90
Shrikant <sawantshrikant07@gmail.com>
2023-03-10 14:30:38 +0530

    [Shrikant] add:WelcomeEmployeeWage

---
+++ EmployeeWageComputationProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EmployeeWageComputationProblem
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem ");
+        }
+    }
+}
